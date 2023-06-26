da2a247ffd6d8b594af3b8fe03eb34df70f236d7
Sturlen <43607012+Sturlen@users.noreply.github.com>
2023-06-26 19:38:07 +0200

    seperate PetEditor component

---
+++ src/c/PetEditor.tsx
@@ -0,0 +1,68 @@
+import { DefaultPet, PetData } from "~/types";
+import Dragoon from "./Dragoon";
+import { useState } from "react";
+import { dragoon_glasses } from "~/items";
+import SignedIn from "./SignedIn";
+import SignedOut from "./SignedOut";
+
+export type PetEditorProps = {
+  initalData?: PetData;
+  onSave?: (newData: PetData) => void;
+};
+
+const PetEditor: React.FC<PetEditorProps> = ({ initalData, onSave }) => {
+  const defaultData = initalData || DefaultPet;
+  const [color, setColor] = useState<string | undefined>(undefined);
+  const [glasses, setGlassesId] = useState<number | undefined>(undefined);
+
+  const data: PetData = {
+    color: color || defaultData.color,
+    glasses: glasses || defaultData.glasses,
+  };
+
+  return (
+    <section className="flex flex-col items-center">
+      <h2>Pet Editor</h2>
+      <Dragoon data={data} />
+
+      <div className="flex flex-col">
+        <label htmlFor="color">Color</label>
+        <input
+          name="color"
+          type="color"
+          value={data.color}
+          onInput={(e) => setColor(e.currentTarget.value)}
+        />
+        <select
+          name="glasses"
+          id="glasses"
+          value={data.color}
+          onInput={(e) => setGlassesId(parseInt(e.currentTarget.value))}
+        >
+          {dragoon_glasses.map((item, i) => {
+            return (
+              <option key={i} value={i.toString()}>
+                {item.src}
+              </option>
+            );
+          })}
+        </select>
+        <SignedIn>
+          <button
+            className="rounded-md border border-black p-2 hover:bg-slate-100"
+            onClick={() => {
+              onSave?.(data);
+            }}
+          >
+            Save
+          </button>
+        </SignedIn>
+        <SignedOut>
+          <p>You need to be logged in to save changes to your pet.</p>
+        </SignedOut>
+      </div>
+    </section>
+  );
+};
+
+export default PetEditor;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { useState } from "react";
 import Dragoon from "~/c/Dragoon";
+import PetEditor from "~/c/PetEditor";
 import SignedIn from "~/c/SignedIn";
 import SignedOut from "~/c/SignedOut";
 import { dragoon_glasses } from "~/items";
@@ -10,13 +12,17 @@ import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const userId = "swag";
-  // const petQuery = api.example.petbyOwnerId.useQuery(userId, {
-  //   placeholderData: DefaultPet,
-  // }); // need to work on loading existing data first
+  const { data: SessionData, status } = useSession();
+
+  const petQuery = api.example.petbyOwnerId.useQuery(
+    SessionData?.user.id || "",
+    {
+      enabled: !!SessionData?.user,
+    }
+  ); // need to work on loading existing data first
   const utils = api.useContext();
 
-  const [color, setColor] = useState("#eeaaee");
-  const [glasses_id, setGlassesId] = useState<number>(0);
+  const pet_data = petQuery.data;
 
   const pet = api.example.pet.useMutation({
     onMutate: async (value) => {
@@ -31,10 +37,6 @@ const Home: NextPage = () => {
       await utils.example.petbyOwnerId.invalidate();
     },
   });
-  const data: PetData = {
-    color: color,
-    glasses: glasses_id,
-  };
 
   return (
     <>
@@ -43,48 +45,17 @@ const Home: NextPage = () => {
         <meta name="description" content="Less neo, more niji" />
       </Head>
       <main className="flex flex-grow flex-col bg-white p-4">
-        <section className="flex flex-col items-center">
-          <h2>Pet Editor</h2>
-          <Dragoon data={data} />
-
-          <div className="flex flex-col">
-            <label htmlFor="color">Color</label>
-            <input
-              name="color"
-              type="color"
-              value={color}
-              onInput={(e) => setColor(e.currentTarget.value)}
-            />
-            <select
-              name="glasses"
-              id="glasses"
-              value={data.glasses}
-              onInput={(e) => setGlassesId(parseInt(e.currentTarget.value))}
-            >
-              {dragoon_glasses.map((item, i) => {
-                return (
-                  <option key={i} value={i.toString()}>
-                    {item.src}
-                  </option>
-                );
-              })}
-            </select>
-            <SignedIn>
-              <button
-                className="rounded-md border border-black p-2 hover:bg-slate-100"
-                onClick={() => {
-                  pet.mutate({ color: data.color, glasses: glasses_id });
-                }}
-                disabled={pet.isLoading}
-              >
-                Save
-              </button>
-            </SignedIn>
-            <SignedOut>
-              <p>You need to be logged in to save changes to your pet.</p>
-            </SignedOut>
-          </div>
-        </section>
+        {SessionData && <PetEditor initalData={pet_data} onSave={pet.mutate} />}
+        {status === "unauthenticated" && (
+          <section>
+            <p>
+              You must be logged in to create a pet. You can try out the editor
+              if ya like:
+            </p>
+            <PetEditor />
+          </section>
+        )}
+        {status === "loading" && <p>:DragoonPause:</p>}
       </main>
     </>
   );
