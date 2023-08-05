c95a132ce9d0c9cb8985ee0a8dcc7306b9086846
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-08-05 21:27:38 +0200

    improved playground

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -1,10 +1,12 @@
 import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import Dragoon from "~/c/Dragoon";
 import { api } from "~/utils/api";
 
 const CreatePet: NextPage = () => {
+  const { data: session, status } = useSession();
   const { data: pets } = api.pets.all.useQuery({});
 
   return (
@@ -15,23 +17,40 @@ const CreatePet: NextPage = () => {
       </Head>
       <main className="w-full bg-white">
         <div className="flex items-center justify-center">
-          {pets && (
-            <ul className="flex flex-row items-center justify-center">
-              {pets.map((pet, i) => (
-                <li key={i}>
-                  <Link href={`/pets/${pet.id}`}>
-                    <Dragoon data={pet.apperance} />
-                    <p>{pet.name}</p>
-                    <p>Owner: {pet.owner}</p>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          )}
+          <ul className="ml-4 mr-4 flex flex-row flex-wrap">
+            {pets && status !== "loading"
+              ? pets.map(({ id, apperance, owner, name }) => (
+                  <li key={id} className="m-1">
+                    <Link href={`/pets/${id}`}>
+                      <Dragoon data={apperance} />
+                      <p>{name}</p>
+                      {session?.user.id == owner ? (
+                        <p>Your pet</p>
+                      ) : (
+                        <p>Owner: {owner}</p>
+                      )}
+                    </Link>
+                  </li>
+                ))
+              : loading_skeletons}
+          </ul>
         </div>
       </main>
     </>
   );
 };
+
+// TODO: standardise for all pet lists
+// TODO: standardise pet image sizes
+const loading_skeletons = Array(16)
+  .fill(undefined)
+  .map((_, i) => (
+    <li className="m-1 block" key={i}>
+      <div className="block h-[128px] w-[128px] rounded-md bg-slate-400"></div>
+      <p className=" mt-1  block w-[128px] rounded-md bg-slate-400 text-transparent">
+        🗿
+      </p>
+    </li>
+  ));
 
 export default CreatePet;
