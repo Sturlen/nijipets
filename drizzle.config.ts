453452b7c91eda6ff7ab7ff4b5fde933e0eb0004
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-08-10 13:05:53 +0200

    update drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,9 @@
 import type { Config } from "drizzle-kit";
+import { z } from "zod";
 import "dotenv/config";
 // import { env } from "~/env.mjs";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = z.string().url().parse(process.env.DATABASE_URL);
 
 if (!connectionString) {
   throw new Error("'DATABASE_URL' env variable is required");
