36a9d42258dec0e9f68bde6425dce42a4597116d
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-09-10 21:52:36 +0200

    signin button fix

--- src/c/SignInButton.tsx
+++ src/c/SignInButton.tsx
@@ -2,6 +2,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { $path } from "next-typesafe-url";
 import Link from "next/link";
 import SignedIn from "./SignedIn";
+import SignedOut from "./SignedOut";
 
 const SignInButton: React.FC = () => {
   const { data: sessionData } = useSession();
@@ -19,31 +20,34 @@ const SignInButton: React.FC = () => {
       )}
       {img_src && <img className="h-8 w-8 rounded-full" src={img_src} alt="" />}
 
-      <Link
-        className="rounded-sm bg-white/10 px-10 py-3 text-4xl font-bold text-black no-underline transition hover:bg-white/20"
-        href={$path({
-          route: "/auth/sign-up",
-          searchParams: { callbackUrl: "/pets" },
-        })}
-      >
-        Sign Up
-      </Link>
-
-      <button
-        className="rounded-sm bg-white/10 px-10 py-3 text-4xl font-bold text-black no-underline transition hover:bg-white/20"
-        onClick={() => void signIn()}
-      >
-        {"Sign in"}
-      </button>
+      <SignedOut>
+        <Link
+          className="rounded-sm bg-white/10 px-10 py-3 text-4xl font-bold text-black no-underline transition hover:scale-105 hover:bg-white/20"
+          href={$path({
+            route: "/auth/sign-up",
+            searchParams: { callbackUrl: "/pets" },
+          })}
+        >
+          Sign Up
+        </Link>
+        <button
+          className="rounded-sm bg-white/10 px-10 py-3 text-4xl font-bold text-black no-underline transition hover:scale-105 hover:bg-white/20"
+          onClick={() => void signIn()}
+        >
+          {"Sign in"}
+        </button>
+      </SignedOut>
 
       <SignedIn>
         <button
-          className="rounded-sm bg-white/10 px-10 py-3 text-4xl font-bold text-black no-underline transition hover:bg-white/20"
+          className="rounded-sm bg-white/10 px-10 py-3 text-4xl font-bold text-black no-underline transition hover:scale-105 hover:bg-white/20"
           onClick={() => void signOut()}
         >
           {"Sign out"}
         </button>
       </SignedIn>
+
+      <div className="mr-8"></div>
     </>
   );
 };
